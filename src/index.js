de0e3c34575f5c46efcf64853073c37c407479be
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-17 23:57:06 +0900

    createStore - redux, refactor #1 (before)

--- src/index.js
+++ src/index.js
@@ -4,25 +4,31 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-// update value
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  countStore.dispatch({ type: "ADD" });
 };
 
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({ type: "MINUS"});
 };
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
-
