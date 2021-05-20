675e4c4c29579d41e8138eeebf600a6fbbb9eeb0
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-20 20:13:51 +0900

    first practise of applying Redux JS - (state, action)

--- src/index.js
+++ src/index.js
@@ -1,42 +1,12 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-// prevent typo mistake by converting them as variable - for JS to detect this.
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
-  //just used switch here as document did.
-  switch (action.type){
-    case ADD:
-      return count + 1
-    case MINUS:
-      return count - 1
-    default:
-      return count;
-  }
-};
-
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-};
-
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
